7f240a744385563eebdb122b54b4abf1d2906551
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-11-02 20:01:59 +0100

    refactor: publisher subscriber pattern between view and controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -19,6 +19,8 @@ const controlRecipes = async function () {
 	}
 };
 
-['hashchange', 'load'].forEach((event) =>
-	window.addEventListener(event, controlRecipes)
-);
+const init = function () {
+	recipeView.addHandlerRender(controlRecipes);
+};
+
+init();
