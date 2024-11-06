1e0952e8bb39fa45c8ab7e3410c9d03b52844912
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-11-06 23:11:23 +0100

    add: search functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,6 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -19,8 +20,22 @@ const controlRecipes = async function () {
 	}
 };
 
+const controlSearchResults = async function () {
+	try {
+		const query = searchView.getQuery();
+
+		if (!query) return;
+
+		await model.loadSearchResults(query);
+		console.log(model.state.search.results);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
 const init = function () {
 	recipeView.addHandlerRender(controlRecipes);
+	searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -4,11 +4,15 @@ import { getJSON } from './helpers';
 
 export const state = {
 	recipe: {},
+	search: {
+		query: '',
+		results: [],
+	},
 };
 
 export const loadRecipe = async function (id) {
 	try {
-		const data = await getJSON(`${API_URL}/${id}`);
+		const data = await getJSON(`${API_URL}${id}`);
 
 		const { recipe } = data.data;
 		state.recipe = {
@@ -22,6 +26,27 @@ export const loadRecipe = async function (id) {
 			ingredients: recipe.ingredients,
 		};
 		console.log(state.recipe);
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+};
+
+export const loadSearchResults = async function (query) {
+	try {
+		state.search.query = query;
+		const data = await getJSON(`${API_URL}?search=${query}`);
+		// console.log(data);
+
+		state.search.results = data.data.recipes.map((recipe) => {
+			return {
+				id: recipe.id,
+				title: recipe.title,
+				published: recipe.publisher,
+				sourceUrl: recipe.source_url,
+				image: recipe.image_url,
+			};
+		});
 	} catch (error) {
 		console.error(error);
 		throw error;
