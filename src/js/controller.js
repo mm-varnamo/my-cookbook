612067554742fa373c22a117ec44e2f02a268283
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-11-13 23:05:56 +0100

    add: pagination function

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,9 +6,9 @@ import resultsView from './views/resultsView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-if (module.hot) {
-	module.hot.accept();
-}
+// if (module.hot) {
+// 	module.hot.accept();
+// }
 
 const controlRecipes = async function () {
 	try {
@@ -35,7 +35,8 @@ const controlSearchResults = async function () {
 
 		await model.loadSearchResults(query);
 
-		resultsView.render(model.state.search.results);
+		// resultsView.render(model.state.search.results);
+		resultsView.render(model.getSearchResultsPage(1));
 	} catch (error) {
 		console.log(error);
 	}
