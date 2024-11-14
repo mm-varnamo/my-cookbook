40d078105b4fad5395090a160eec11845f169667
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-11-14 23:16:06 +0100

    add: pagination button browsing functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -36,15 +37,24 @@ const controlSearchResults = async function () {
 		await model.loadSearchResults(query);
 
 		// resultsView.render(model.state.search.results);
-		resultsView.render(model.getSearchResultsPage(1));
+		resultsView.render(model.getSearchResultsPage());
+		paginationView.render(model.state.search);
 	} catch (error) {
 		console.log(error);
 	}
 };
 
+const controlPagination = function (goToPage) {
+	// Render updated results
+	resultsView.render(model.getSearchResultsPage(goToPage));
+	// Render updated pagination buttons
+	paginationView.render(model.state.search);
+};
+
 const init = function () {
 	recipeView.addHandlerRender(controlRecipes);
 	searchView.addHandlerSearch(controlSearchResults);
+	paginationView.addHandlerClick(controlPagination);
 };
 
 init();
